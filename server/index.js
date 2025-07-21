7ccfbaeff669c9f662661a1d38d6d050d24fe084
Mohan <167054270+mohan-bee@users.noreply.github.com>
2025-07-21 23:51:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const app=express()
 const PORT=process.env.PORT || 5000
 const cors =require('cors')
 const connectdb=require('./config/db')
-const router=require('./routes/routes')
+const songRoutes = require('./routes/routes')
 
 require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
-app.use('/api',router)
+app.use('/api/song',songRoutes)
 
 
 app.listen(PORT,async()=>{

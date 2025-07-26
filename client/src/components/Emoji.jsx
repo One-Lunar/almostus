d54a4658ab46d2162f510c80b17fee76ebf0aacd
mohan-bee <mohn08052006@gmail.com>
2025-07-26 19:07:39 +0530

    fix: chore

--- client/src/components/Emoji.jsx
+++ client/src/components/Emoji.jsx
@@ -35,33 +35,34 @@ const Emoji = ({roomId, username}) => {
 
   return (
     <div>
-        <div className='fixed right-50 bottom-10'>
-             <AnimatePresence>
-        {bubbles.map((bubble) => (
-          <motion.div
-            key={bubble.id}
-            initial={{ opacity: 1, y: 0, x: 0, rotate: 0, scale: 1 }}
-            animate={{
-              opacity: 0,
-              y: -150,
-              x: bubble.x,
-              scale: 1.3,
-            }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 1.5 }}
-            className="absolute bottom-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center pointer-events-none"
-            onAnimationComplete={() =>
-              setBubbles((prev) => prev.filter((b) => b.id !== bubble.id))
-            }
-          >
-            <span className="text-4xl">{bubble.emoji}</span>
-            <span className="text-xs w-full bg-zinc-800 rounded-xl p-1 border border-zinc-600 mt-1">
-              {bubble.username}
-            </span>
-          </motion.div>
-        ))}
-      </AnimatePresence>
-        </div>
+        <div className="fixed right-12 bottom-10">
+  <AnimatePresence>
+    {bubbles.map((bubble) => (
+      <motion.div
+        key={bubble.id}
+        initial={{ opacity: 1, y: 0, x: -400, scale: 1 }}
+        animate={{
+          opacity: 0,
+          y: -250,
+          x: bubble.x-400,
+          scale: 1.3,
+        }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.5 }}
+        className="absolute bottom-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center pointer-events-none"
+        onAnimationComplete={() =>
+          setBubbles((prev) => prev.filter((b) => b.id !== bubble.id))
+        }
+      >
+        <span className="text-4xl">{bubble.emoji}</span>
+        <span className="text-xs bg-zinc-800 rounded-xl p-1 border border-zinc-600 mt-1">
+          {bubble.username}
+        </span>
+      </motion.div>
+    ))}
+  </AnimatePresence>
+</div>
+
          <div className="fixed flex gap-4 items-center bottom-25 right-5 bg-zinc-800 border border-zinc-700 px-4 py-2 rounded-full">
         {isOpen && (
           <ul className="flex text-2xl gap-3 items-center">
